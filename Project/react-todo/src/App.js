36c77bbb8dca5c5ab446ad00b3818f27cae89d3c
fftl <fftl.0710@gmail.com>
2020-08-08 10:42:37 +0900

    todo app finish

--- Project/react-todo/src/App.js
+++ Project/react-todo/src/App.js
@@ -13,6 +13,8 @@ class App extends Component {
     }
     this._handleOnClickAddItem = this._handleOnClickAddItem.bind(this)
     this._handleOnChangeTodoInput = this._handleOnChangeTodoInput.bind(this)
+    this._handleOnClickToggleState = this._handleOnClickToggleState.bind(this)
+    this._handleOnClickRemove = this._handleOnClickRemove.bind(this)
   }
 
   _handleOnClickAddItem (){
@@ -34,7 +36,29 @@ class App extends Component {
     this.setState({ todoInput : e.target.value })
   }
 
+  _handleOnClickToggleState (index) {
+    const { todoItems } = this.state
+    const newTodoItems = todoItems.slice(0)
+    newTodoItems[index].isCompleted = !todoItems[index].isCompleted
+    this.setState({ todoItems: newTodoItems })
+  }
+  
+  _handleOnClickRemove (id) {
+    const { todoItems } = this.state
+    const newTodoItems = todoItems.filter(item => item.id !== id)
+    this.setState({ todoItems: newTodoItems })
+  }
+
   render() {
+    const renderCancelButton = item =>(
+      <button
+        class='btn btn-danger btn-sm'
+        style={{ marginLeft : 5 }}
+        onClick={() => this._handleOnClickRemove(item.id)}
+      >
+        삭제
+      </button>
+    )
     return (
       <div className='container' style={{ maxWidth: 600, padding: '20px 0'}}>
         <div className='row'>
@@ -57,6 +81,42 @@ class App extends Component {
                 </button>
               </div>
             </div>
+          </div>
+        </div>
+        <div className='row' style={ { marginTop: 20 }}>
+          <div className='col-6'>
+            <h3>해야할 일</h3>
+              {
+                this.state.todoItems.filter( item => !item.isCompleted).map(item =>
+                  <div key={item.id} style={{ margin: 10}}>
+                    <span style={{ marginRight : 5 }}>- {item.title}</span>
+                    <button
+                      className='btn btn-success btn-sm'
+                      onClick={() => this._handleOnClickToggleState(item.id)}
+                      >
+                      완료
+                    </button>
+                    {renderCancelButton(item)}
+                    </div>
+                  )
+                }
+          </div>
+          <div className='col-6'>
+            <h3>완료한 일</h3>
+            {
+              this.state.todoItems.filter(item => item.isCompleted).map(item =>
+                <div key={item.id} style={{ margin: 10}}>
+                  <span style={{ marginRight : 5}}>- {item.title}</span>
+                  <button
+                    className='btn btn-warning btn-sm'
+                    onClick={() => this._handleOnClickToggleState(item.id)}
+                    >
+                      취소
+                    </button>
+                    {renderCancelButton(item)}
+                  </div>
+                )
+            }
           </div>
         </div>
       </div>
