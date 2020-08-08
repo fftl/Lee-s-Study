1816f2ba4642b5e6c7e02d1ae2abcc2e91a922de
fftl <fftl.0710@gmail.com>
2020-08-08 10:47:14 +0900

    200808 push

--- algorithm_study/testing/public/js/test.js
+++ algorithm_study/testing/public/js/test.js
@@ -1,44 +1,34 @@
-function solution(board, moves) {
-    
-    var answer = 0;
-    let basket = new Array;
+function solution(participant, completion) {
+    var answer = '';
 
-    for(let a=0; a<moves.length; a++){
-        console.log(a+'############################################')
+    //try1
+    // for(let a=0; a<participant.length; a++){
+    //     if(completion.indexOf(participant[a]) == -1){
+    //         answer=participant[a];
+    //         break;
+    //     } else {
+    //         completion.splice(completion.indexOf(participant[a]), 1)
+    //     }
 
-        for(let b=0; b<board.length; b++){
-            let boardindex = moves[a]-1
-            if (board[b][boardindex] == 0){
-                continue
-            } else {
-                console.log('값'+board[b][boardindex])
-                if(basket.length >0){
+    // }
+    //try2
+    // participant.sort()
+    // completion.sort()
+    // while(true){
+        //     if(participant[0] == completion[0]){
+            //         participant.splice(0, 1)
+            //         completion.splice(0, 1)
+            //     } else {
+                //         answer= participant[0]
+                //         break;
+                //     }
+                // }
+    participant.sort()
+    completion.sort()
 
-                    let basketindex = basket.length -1
-                    if(basket[basketindex] == board[b][boardindex]){
-                        answer = answer + 2;
-                        basket.pop();
-                    } else {
-                        basket[basketindex+1] = board[b][boardindex];
-                    }
-
-                    board[b][boardindex] = 0;
-
-                    break;
-                } else {
-                    
-                    basket[0] = board[b][boardindex];
-                    board[b][boardindex] = 0;
-                    break;
-                }
-            }
-        }
-
-        console.log(basket)
-    }
-    
-    console.log(answer)
+    let test = participant - completion
+    console.log(test)
     return answer;
 }
 
-solution([[0, 0, 0, 0, 0], [0, 0, 1, 0, 3], [0, 2, 5, 0, 1], [4, 2, 4, 4, 2], [3, 5, 1, 3, 1]], [1, 5, 3, 5, 1, 2, 1, 4]);
+solution(	["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
