69a05707ab59bb8e2622fca9b62dd853aa2a47de
fftl <fftl.0710@gmail.com>
2020-08-12 22:03:06 +0900

    200812, study

--- algorithm_study/testing/public/js/test.js
+++ algorithm_study/testing/public/js/test.js
@@ -1,34 +1,22 @@
-function solution(participant, completion) {
-    var answer = '';
+function solution(skill, skill_trees) {
+    var answer = 0;
 
-    //try1
-    // for(let a=0; a<participant.length; a++){
-    //     if(completion.indexOf(participant[a]) == -1){
-    //         answer=participant[a];
-    //         break;
-    //     } else {
-    //         completion.splice(completion.indexOf(participant[a]), 1)
-    //     }
-
-    // }
-    //try2
-    // participant.sort()
-    // completion.sort()
-    // while(true){
-        //     if(participant[0] == completion[0]){
-            //         participant.splice(0, 1)
-            //         completion.splice(0, 1)
-            //     } else {
-                //         answer= participant[0]
-                //         break;
-                //     }
-                // }
-    participant.sort()
-    completion.sort()
-
-    let test = participant - completion
-    console.log(test)
+    console.log('for start')
+    for(let a=0; a<skill_trees.length; a++){
+        let skill_array = new Array();
+        console.log(a)
+        for(let b=0; b<skill.length; b++){
+            skill_array.push(skill_trees[a].indexOf(skill[b]))
+        }
+        console.log(skill_array)
+        // if(skill_array[0] == -1){
+        //     continue
+        // } else {
+        //     for(let c=0; c<skill.length; c++){
+        //         if(skill_array[c]>skill)
+        //     }
+        // }
+    }
     return answer;
 }
-
-solution(	["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
+solution("CBD",["BACDE", "CBADF", "AECB", "BDA"])
