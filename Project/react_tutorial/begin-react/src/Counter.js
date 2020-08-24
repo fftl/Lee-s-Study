61938fa4fce16d09efb3e44f7ae780c7288414b4
fftl <fftl.0710@gmail.com>
2020-08-24 22:19:08 +0900

    new react tutorial start

---
+++ Project/react_tutorial/begin-react/src/Counter.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [number, setNumber] = useState(0);
+    const onIncrease = () =>{
+        setNumber(prevNumber => prevNumber + 1);
+    }
+    const deIncrease = () =>{
+        setNumber(prevNumber => prevNumber - 1);
+    }
+    return(
+        <div>
+            <h1>{number}</h1>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={deIncrease}>-1</button>
+        </div>
+    )
+}
+
+export default Counter;
---
+++ Project/react_tutorial/begin-react/src/InputSample.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function InputSample() {
+    const onChange = (e) => {
+        console.log(e.target.value);
+    }
+    return(
+        <div>
+            <input onChange={onChange}/>
+            <button>초기화</button>
+            <div>
+                <b>값: </b>
+                어쩌고저쩌고..
+            </div>
+        </div>
+    )
+}
+
+export default InputSample;
