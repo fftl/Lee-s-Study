71b05ea2b8b8a5a3991504f8efc48b28fc10325c
fftl <fftl.0710@gmail.com>
2020-08-30 18:09:39 +0900

    2020-08-30

--- Project/react_tutorial/begin-react/src/App.js
+++ Project/react_tutorial/begin-react/src/App.js
@@ -1,9 +1,90 @@
-import React from 'react';
+import React, { useRef, useState, useMemo, useCallback  }  from 'react';
 import InputSample from './InputSample';
+import UserList from './UserList';
+import CreateUser from './CreateUser';
 
+function countActiveUsers(users) {
+  console.log('활성 사용자 수를 세는중...');
+  return users.filter(user=> user.active).length;
+}
 function App() {
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  })
+  const { username, email } = inputs;
+
+  const onChange = useCallback (e =>{
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    })
+  }, [inputs]);
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true,
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false,
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false,
+    },
+  ])
+
+  const nextId = useRef(4);
+
+  const onCreate = useCallback(() => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    }
+    setUsers(users => users.concat(user)); //
+    setInputs({
+      useraname: '',
+      email: '',
+    });
+    console.log(nextId.current);
+    nextId.current += 1;
+  }, [username, email, users]);
+
+  const onRemove = useCallback(id =>{
+    setUsers(users => users.filter(user => user.id !== id));
+  }, []);
+
+  const onToggle = useCallback(id =>{
+    setUsers(users => users.map(
+      user => user.id === id
+        ? {...user, active: !user.active}
+        :user 
+    ));
+  }, []);
+
+  const count = useMemo(()=> countActiveUsers(users), [users]); // 첫번째 파라미터, 함수형태 이 함수를 실행 두번째 파라미터, 객체? 해당 객체가 변경될때 실행
+
   return (
-    <InputSample />
+    <>
+    <CreateUser 
+      username={username} 
+      email={email} 
+      onChange={onChange} 
+      onCreate={onCreate}
+    />
+    <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+    <div>활성 사용자 수: {count}</div>
+    </>
   )
 }
 
--- Project/react_tutorial/begin-react/src/InputSample.js
+++ Project/react_tutorial/begin-react/src/InputSample.js
@@ -1,16 +1,50 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 function InputSample() {
+    const [inputs, setInputs] = useState({
+        name: '',
+        nickname: '',
+    })
+    const nameInput = useRef();
+    const {name , nickname} = inputs;
+
     const onChange = (e) => {
-        console.log(e.target.value);
+        const { name, value } = e.target;
+        setInputs({
+            ...inputs, // 기존의 inputs 객체를 복사해 옵니다.
+            [name]: value,
+        })
+    }
+
+    const onReset = () =>{
+        setInputs({
+            name: '',
+            nickname: ''
+        })
+        nameInput.current.focus(); //current가 해당 돔을 가리켜줍니다.
     }
     return(
         <div>
-            <input onChange={onChange}/>
-            <button>초기화</button>
+
+            <input 
+            name="name" 
+            placeholder="이름" 
+            onChange={onChange} 
+            value={name}
+            ref={nameInput} //이렇게 ref 를 넣어놔 주면 나중에 다른 곳에서 돔을 선택할 수 있게됩니다.
+            />
+
+            <input 
+            name="nickname" 
+            placeholder="닉네임" 
+            onChange={onChange} 
+            value={nickname}
+            />
+
+            <button onClick={onReset}>초기화</button>
             <div>
                 <b>값: </b>
-                어쩌고저쩌고..
+                {name}({nickname})
             </div>
         </div>
     )
