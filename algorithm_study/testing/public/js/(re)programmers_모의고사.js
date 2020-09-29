ad5f1e57989e584daf6581bebeeada485d9323e6
fftl <fftl.0710@gmail.com>
2020-09-29 15:52:16 +0900

    2020-09-29, 모의고사 풀이

--- algorithm_study/testing/public/js/(re)programmers_모의고사.js
+++ algorithm_study/testing/public/js/(re)programmers_모의고사.js
@@ -8,18 +8,21 @@ function solution(ans) {
     let supo1 = [];
     let count1 = 0;
     let yesCount1 =0;
-    while( count1 !== ans.length ){
+    while( count1 !== ans.length ){  //이 while 을 통해서 시험문제 개수와 동일한 수의 수포자 패턴 답을 만듭니다.
         for(let a=0; a<supo1_pt.length; a++){
             supo1.push(supo1_pt[a]);
             count1++
+            if(count1 === ans.length){ //개수가 같아지면 중지
+                break;
+            }
         }
     }
-    for(let a=0; a<ans.length; a++){
+    for(let a=0; a<ans.length; a++){ //체점입니다. 동일한 인덱스에 동일한 값 즉 정답일 경우 yesCount1 를 증가시켜줍니다.
         if(ans[a] === supo1[a]){
             yesCount1++
         }
     }
-    answer.push(yesCount1)
+    answer.push(yesCount1) //정답 개수를 배열에 입력해 줍니다.
     //supo2 #############################################
     let supo2 = [];
     let count2 = 0;
@@ -28,6 +31,9 @@ function solution(ans) {
         for(let a=0; a<supo2_pt.length; a++){
             supo2.push(supo2_pt[a]);
             count2++
+            if(count2 === ans.length){
+                break;
+            }
         }
     }
 
@@ -45,6 +51,9 @@ function solution(ans) {
         for(let a=0; a<supo3_pt.length; a++){
             supo3.push(supo3_pt[a]);
             count3++
+            if(count3 === ans.length){
+                break;
+            }
         }
     }
     for(let a=0; a<ans.length; a++){
@@ -54,12 +63,21 @@ function solution(ans) {
     }
     answer.push(yesCount3)
 
+    //최고 득점자 찾기 
+    let maxNum = Math.max.apply(null, answer) // 배열내에서 최대값 구하기
+    let dap = [];
+
+    for(let a=0; a<3; a++){
+        if(answer[a] === maxNum){ //최대값인 경우에는 해당 인덱스를 push 해줍니다.
+            dap.push(a+1)
+        }
+    }
     
+    dap.sort((a, b) => a-b) // 배열의 수를 오름차순으로 정렬
 
 
-
-    console.log(answer);
-    return answer;
+    console.log(dap);
+    return dap;
 }
 
 
